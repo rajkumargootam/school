7e74bc0da78d07ae95788bde0bb924d65f79f028
rajkumar <rajkumar.gootam@gmail.com>
2016-03-04 01:46:00 +0530

    displayed the title in the klasses and added Edit icon to the klass table

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -1,4 +1,15 @@
+# == Schema Information
+#
+# Table name: klasses
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class KlassesController < ApplicationController
   def index
+    @all_classes = Klass.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303162810) do
+ActiveRecord::Schema.define(version: 20160303191414) do
 
   create_table "klasses", force: :cascade do |t|
     t.string   "title"
